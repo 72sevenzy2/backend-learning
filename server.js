058b3d0124e3c5e427a16d568d3c067104793476
72 <72sevenzy2@gmail.com>
2025-09-19 22:21:24 -0800

    made a users api router

--- server.js
+++ server.js
@@ -3,14 +3,31 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT;
 
+const users = [
+    { name: "john doe", id: 1 },
+    { name: "jahn doe", id: 2 },
+    { name: "jane", id: 3 }
+]
+
 const server = http.createServer((req, res) => {
-    if (req.url === '/server.htm') {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.end("<h1>test</h1>");
-    } else if (req.url === '/server.htm/about') {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.end("<h1>about</h1>");
-    } else {
+    if (req.url === '/api/users' && req.method === 'GET') {
+        res.setHeader("content-type", "application/json");
+        res.write(JSON.stringify(users)); res.end();
+    }
+    else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+        const id = req.url.split('/')[3];
+        const user = users.find((user) => user.id === parseInt(id));
+        if (user) {
+            res.setHeader("content-type", "application/json");
+            res.write(JSON.stringify(user));
+            res.end();
+        }
+        else {
+            res.writeHead(404, { "content-type": "application/json" });
+            res.end(JSON.stringify({ message: "user not found" }));
+        }
+    }
+    else {
         res.writeHead(404, { "content-type": "text/html" });
         res.end("<h1>not found</h1>");
     }

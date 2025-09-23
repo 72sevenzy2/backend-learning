163674b54e555e607225f21b92a47af2e9c3644a
72 <72sevenzy2@gmail.com>
2025-09-23 10:18:26 -0800

    added major parts to routes directory for a more cleanup

--- express.js/app.js
+++ express.js/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { routes } from "./routes.js";
+import router from "../routes/routes.js";
+
+const posts = router;
 
 const app = express();
 const port = process.env.PORT;
@@ -9,19 +12,6 @@ const port = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const posts = [
-        { id: 1, title: "hello world" },
-        { id: 2, title: "hello java" },
-        { id: 3, title: "hello six seveeeeeeen" }
-];
-
-app.get("/api/posts", (req, res) => {
-        res.json(posts);
-});
-
-app.get("/api/posts/:id", (req, res) => {
-        const id = parseInt(req.params.id);
-        res.json(posts.filter((post) => post.id == id));
-});
+app.use("/api/posts", posts);
 
 app.listen(port, () => { console.log("sever is running" + port) });

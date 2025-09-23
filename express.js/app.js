65b92cd3856bbfc67a1f12c5657506884de796f9
72 <72sevenzy2@gmail.com>
2025-09-23 08:09:05 -0800

    added req params

--- express.js/app.js
+++ express.js/app.js
@@ -1,10 +1,27 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { routes } from "./routes.js";
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-        res.send({message: "hello world"});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const posts = [
+        { id: 1, title: "hello world" },
+        { id: 2, title: "hello java" },
+        { id: 3, title: "hello six seveeeeeeen" }
+];
+
+app.get("/api/posts", (req, res) => {
+        res.json(posts);
+});
+
+app.get("/api/posts/:id", (req, res) => {
+        const id = parseInt(req.params.id);
+        res.json(posts.filter((post) => post.id == id));
 });
 
 app.listen(port, () => { console.log("sever is running" + port) });

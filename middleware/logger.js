35fbeb2de9e24f44600a895f74eb2d79931ca939
72 <72sevenzy2@gmail.com>
2025-09-24 06:50:00 -0800

    added the colors package

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,5 +1,14 @@
+import colors from "colors";
+
 const logger = (req, res, next) => {
-    console.log(`${req.method} | ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    const methods = {
+        GET: "green",
+        POST: "blue",
+        PUT: "yellow",
+        DELETE: "red"
+    }
+    const color = methods[req.method] || white;
+    console.log(`${req.method} | ${req.protocol}://${req.get('host')}${req.originalUrl}`[color]);
     next();
 }
 
